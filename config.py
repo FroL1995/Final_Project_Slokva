502634ae2b6e07cc2a6cbd8d0d7c91e9321b422c
FedorUltraz <zhfedor33@gmail.com>
2025-07-13 20:43:22 +0300

    /

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = '7425570534:AAGlYv-Tp30IgfjW5K1lOalcJvrY_9X2O8g'
-STEAM_API_KEY = '34bbaac3f9msh1c41c9ed3fe6b47p176650jsn8df5cefe164d'
+STEAM_API_KEY = '2e9710c0f7mshcc8cc90b1fa9ec4p114759jsnbbe0a0536b61'
 DB_PATH = "db/database.db"
 API_BASE_URL = "https://steam2.p.rapidapi.com"
 
